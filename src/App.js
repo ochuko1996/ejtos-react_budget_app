86c7c9cf3200c0a104f9dc3fd4a1f020db8f73df
ochuko1996 <georgeochuko7@.com>
2024-05-04 13:56:09 -0400

    budgetallocation

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm'
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
 const App = () => {
     return (
         <AppProvider>
@@ -17,26 +21,41 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                                            // Budget component
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                             //Remaining component
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                            //ExpenseTotal component
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <ExpenseList />
                         }         
 
                         {
                             /* Add ExpenseItem component here */
+                            <ExpenseItem/>
                         }        
 
                         {
                             /* Add AllocationForm component here under */
+                            <AllocationForm/>
                         }        
 
                 </div>
