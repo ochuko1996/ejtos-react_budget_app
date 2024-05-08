b24f1a90a2785593d24ee1efe4af3c36fed8b9f9
ochuko1996 <georgeochuko7@.com>
2024-05-08 17:20:28 -0400

    completed budget update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,8 +2,11 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch, expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
 
     const handleBudgetChange = (event) => {
         const parseData =  parseInt(event.target.value)
@@ -11,7 +14,13 @@ const Budget = () => {
             alert("budget can't exceed 20,000")
             return;
         }
+        if (budget <= totalExpenses) {
+            alert("you cannot reduce the budget lower than the spending")
+            return;
+            
+        }
         setNewBudget(parseData);
+        dispatch({ type: 'SET_BUDGET', payload: parseData });
     }
     return (
 <div className='alert alert-secondary'>
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Remaining = () => {
     const { expenses, budget } = useContext(AppContext);
+
     const totalExpenses = expenses.reduce((total, item) => {
         return (total = total + item.cost);
     }, 0);
