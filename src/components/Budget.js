0e29b102d723dffd1d26a2e499cc3924c8738de7
ochuko1996 <georgeochuko7@.com>
2024-05-08 16:19:16 -0400

    updated budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,13 +4,21 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const parseData =  parseInt(event.target.value)
+        if (parseData > 20000) {
+            alert("budget can't exceed 20,000")
+            return;
+        }
+        setNewBudget(parseData);
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: £
+    {/* {budget} */}
+</span>
+<input type="number" step="10" min={0} max={200010} value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
